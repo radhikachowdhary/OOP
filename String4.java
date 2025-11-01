7d941ddfbdc5aee77c3a18bb4db9975ee5f9a17b
radhikachowdhary <radhikachowdhary5531@gmail.com>
2025-11-01 02:42:10 -0700

    Fix string method calls and output statements

---
+++ String4.java
@@ -0,0 +1,14 @@
+public class StringMethods Demo { public static void main(String[] args) {
+
+String s = "Programming";
+
+// Find the first occurrence of 'g' int firstG = s.indexOf('g');
+  System.out.println("First occurrence of 'g': " + firstG);
+
+// Find the last occurrence of 'g' int lastG = s.lastIndexOf('g');
+  System.out.println("Last occurrence of 'g': " + lastG);
+
+// Check if the string contains "gram"
+  boolean containsGram = System.out.println("Contains s.contains("gram"); 'gram': " + containsGram);
+}
+}
