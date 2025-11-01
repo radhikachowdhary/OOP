bc196f48d8b4a164cff20a81a2f1ac5a782a93c4
radhikachowdhary <radhikachowdhary5531@gmail.com>
2025-11-01 02:30:46 -0700

    Add StringCaseExample class for case conversion

---
+++ String3.java
@@ -0,0 +1,11 @@
+public class StringCaseExample {
+
+public static void main(String[]args) {
+
+String s = "radhika";
+
+System.out.println(s.toUpperCase()); //
+
+RADHIKA
+
+System.out.println(s.toLowerCase()); //radhika } }
